855a651d7a351913b3ff2b3fa43e492f6e04761d
lmyers3 <lindsey5535@DESKTOP-318U2V7.localdomain>
2021-11-07 13:23:16 -0500

    created layout to add individual scripts

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+
+var dataset;
+
+d3.csv("../Engineering_graduate_salary.csv").then(function(data) {
+    dataset = data;
+    console.log(dataset)
+
+
+    //enter graph gen functions here ...
+}); 
+
+
